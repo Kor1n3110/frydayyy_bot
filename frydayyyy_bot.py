0832ee9bd869618f1f277daf6a99b12d7185f824
Kor1n3110 <96489470+Kor1n3110@users.noreply.github.com>
2023-04-12 00:08:35 +0300

    Update frydayyyy_bot.py

--- frydayyyy_bot.py
+++ frydayyyy_bot.py
@@ -4,8 +4,8 @@ import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup
 from telegram import ReplyKeyboardRemove
+from random import randint
 from email.mime import application
-
 # Запускаем логгирование
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -33,7 +33,7 @@ Actors = []
 Genre1 = []
 Movie_deteils1 = []
 Actors1 = []
-
+nasmeshka = ["Не понимаю, что вы выбрали, если вы ничего не выбрали... хотя ладно, мне никогда вас не понять", 'Вы же ничего не выбрали...', 'Вы шутите так?', 'Я промолчу...']
 
 # *******
 
@@ -63,7 +63,10 @@ async def help_command(update, context):
 async def actors_command(update, context):
     global COMMAND
     """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text('Ты указываешь главных героев кино')
+    await update.message.reply_text(
+        'Укажите главных героев кино',
+        reply_markup=ReplyKeyboardRemove()
+    )
     COMMAND.append('Actors')
 
 
@@ -81,7 +84,10 @@ async def genre_command(update, context):
 async def movie_details_command(update, context):
     global COMMAND
     """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text('Пишешь, что ты хочешь видеть в кино')
+    await update.message.reply_text(
+        'Заишите, что вы хотите видеть в кино',
+        reply_markup=ReplyKeyboardRemove()
+    )
     COMMAND.append('Movie_deteils')
 
 
@@ -114,20 +120,50 @@ async def keyboard(update, context):
 
 async def close_keyboard(update, context):
     await update.message.reply_text(
+        '',
         reply_markup=ReplyKeyboardRemove()
     )
 
 
 async def MOGHO(update, context):
     global Genre, COMMAND, Movie_deteils, Actors, genre_FLAG, keyboard_FLAG, Genre1, Movie_deteils1, Actors1
-    if update.message.text == 'ВЫБРАЛ':
+    if update.message.text.upper() == 'ВЫБРАЛ':
         Genre = Genre1
         Movie_deteils = Movie_deteils1
         Actors = Actors1
-        await update.message.reply_text(
-            "Замечательно, вы выбрали жанр своего кино.",
-            reply_markup=markup
-        )
+        if COMMAND[-1] == 'Genre':
+            if not Genre1 == []:
+                await update.message.reply_text(
+                    "Замечательно, вы выбрали жанр своего кино.",
+                    reply_markup=markup
+                )
+            else:
+                await update.message.reply_text(
+                    nasmeshka[randint(0, 3)],
+                    reply_markup=markup
+                )
+        elif COMMAND[-1] == 'Actors':
+            if not Actors1 == []:
+                await update.message.reply_text(
+                    "Замечательно, вы выбрали актёров, ираюющих в вашем кино.",
+                    reply_markup=markup
+                )
+            else:
+                await update.message.reply_text(
+                    nasmeshka[randint(0, 3)],
+                    reply_markup=markup
+                )
+        else:
+            if not Movie_deteils1 == []:
+                await update.message.reply_text(
+                    "Замечательно, вы выбрали детали вашего кино.",
+                    reply_markup=markup
+                )
+            else:
+                await update.message.reply_text(
+                    nasmeshka[randint(0, 3)],
+                    reply_markup=markup
+                )
         if not Actors == []:
             a = str(', '.join(Actors))
         else:
@@ -146,7 +182,7 @@ async def MOGHO(update, context):
 Детали: {c}''')
     if COMMAND[-1] == 'Actors':
         if update.message.text not in Actors1 and not (
-                update.message.text == 'ВЫБРАЛ' or update.message.text == 'СБРОС ПАРАМЕТРА' or update.message.text == 'СБРОСИТЬ ПАРАМЕТР И ВЫЙТИ'):
+                update.message.text.upper() == 'ВЫБРАЛ' or update.message.text.upper() == 'СБРОС ПАРАМЕТРА' or update.message.text.upper() == 'СБРОСИТЬ ПАРАМЕТР И ВЫЙТИ'):
             Actors1.append(update.message.text)
             a = 'Вы выбрали: ' + ', '.join(Actors1)
             await update.message.reply_text(str(a))
@@ -154,7 +190,7 @@ async def MOGHO(update, context):
             await update.message.reply_text('Вы уже добавили этого актёра')
     elif COMMAND[-1] == 'Genre':
         if update.message.text not in Genre1 and not (
-                update.message.text == 'ВЫБРАЛ' or update.message.text == 'СБРОС ПАРАМЕТРА' or update.message.text == 'СБРОСИТЬ ПАРАМЕТР И ВЫЙТИ'):
+                update.message.text.upper() == 'ВЫБРАЛ' or update.message.text.upper() == 'СБРОС ПАРАМЕТРА' or update.message.text.upper() == 'СБРОСИТЬ ПАРАМЕТР И ВЫЙТИ'):
             Genre1.append(update.message.text)
             a = 'Вы выбрали: ' + ', '.join(Genre1)
             await update.message.reply_text(str(a))
@@ -162,13 +198,13 @@ async def MOGHO(update, context):
             await update.message.reply_text('Вы уже добавили этот жанр')
     else:
         if update.message.text not in Movie_deteils1 and not (
-                update.message.text == 'ВЫБРАЛ' or update.message.text == 'СБРОС ПАРАМЕТРА' or update.message.text == 'СБРОСИТЬ ПАРАМЕТР И ВЫЙТИ'):
+                update.message.text.upper() == 'ВЫБРАЛ' or update.message.text.upper() == 'СБРОС ПАРАМЕТРА' or update.message.text.upper() == 'СБРОСИТЬ ПАРАМЕТР И ВЫЙТИ'):
             Movie_deteils1.append(update.message.text)
             a = 'Вы выбрали: ' + ', '.join(Movie_deteils1)
             await update.message.reply_text(str(a))
         elif update.message.text in Movie_deteils1:
             await update.message.reply_text('Вы уже добавили эту деталь')
-    if update.message.text == 'СБРОС ПАРАМЕТРА':
+    if update.message.text.upper() == 'СБРОС ПАРАМЕТРА':
         if COMMAND[-1] == 'Genre':
             Genre = []
         elif COMMAND[-1] == 'Actors':
@@ -176,7 +212,7 @@ async def MOGHO(update, context):
         else:
             Movie_deteils = []
         await update.message.reply_text('Параметр сброшен')
-    if update.message.text == 'СБРОСИТЬ ПАРАМЕТР И ВЫЙТИ':
+    if update.message.text.upper() == 'СБРОСИТЬ ПАРАМЕТР И ВЫЙТИ':
         if COMMAND[-1] == 'Genre':
             Genre1 = []
         elif COMMAND[-1] == 'Actors':
@@ -229,3 +265,4 @@ def main():
 # Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
+
